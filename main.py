bdbf54db552943302cf3909c56f60a2849f2c442
Urs Kneifel <urskneifel@gmail.com>
2024-09-25 20:18:24 +0200

    Code Cleanup

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-grades = []
-count = int(input("Wie viele Noten möchtest du eingeben? "))
-for i in range(1, count+1):
-    grade = int(input("Nenne deine "+str(i)+". Note "))
-    grades.append(grade)
-average = sum(grades) / count
-print("Dein Notendurchschnitt ist "+str(average))
+def gradeAverage():
+    grades = []
+    count = int(input("Wie viele Noten möchtest du eingeben? "))
+    for i in range(1, count+1):
+        grade = int(input("Nenne deine "+str(i)+". Note "))
+        grades.append(grade)
+    average = sum(grades) / count
+    print("Dein Notendurchschnitt ist "+str(average))
+
+if __name__ == "__main__":
+    gradeAverage()
